6e72c1becbb932096f7418f646caa81bd79c2c5f
MonetND <45937083+MonetND@users.noreply.github.com>
2022-03-03 19:27:43 +0800

    Add files via upload

--- build/Lede_source/files/etc/1clash.sh
+++ build/Lede_source/files/etc/1clash.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/MonetND/common/main/config.yaml -O /etc/openclash/config/config.yaml&&
-cp -p /etc/openclash/config/* /etc/openclash/proxy_provider
+cp -p /etc/openclash/config/* /etc/openclash/proxy_provider&&
+cp -p /etc/openclash/backup/可乐云.yaml /etc/openclash/proxy_provider
